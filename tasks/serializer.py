1d8dad18c830c1be3c948cfe75bc50ddc0fff67a
nicsanmen <nicsanmen@alum.us.es>
2023-06-13 19:53:27 +0200

    commit

---
+++ tasks/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'description', 'completed')
+        # fiedls = '__all__' para selializar todos los campos
