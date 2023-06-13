7b38be554a1aafefde0590763824e01adddef172
nicsanmen <nicsanmen@alum.us.es>
2023-06-13 17:04:57 +0200

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500, blank=True)
+    completed = models.BooleanField(default=False)
