026abfc1be071ff6821fa67938183e6bc34cfc82
nicsanmen <nicsanmen@alum.us.es>
2023-07-21 13:59:28 +0200

    actualizado en MAC + requirements.txt

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
+// CREATE
 const tasksApi = axios.create({
     baseURL: 'http://localhost:8000/tasks/api/v1/tasks/',
 })
 
+// GET
 export const getAllTasks = () => {
     // return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
     return tasksApi.get('/');
 }
 
+// UPDATE (?)>>
 export const createTask = (task) => {
     // return axios.post('http://localhost:8000/tasks/api/v1/tasks/', task)
     return tasksApi.post('/', task);
