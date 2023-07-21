fa6a19d4e33f318ee4e61be6e54e936364af512f
nicsanmen <nicsanmen@alum.us.es>
2023-07-21 17:51:58 +0200

    arreglos

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Navigation() {
   return (
-    <div className="flex justify-between py-3 items-center">
+    <div className="flex justify-between px-3 py-3 items-center">
       <Link to="/tasks">
         <h1 className="font-bold text-3xl mb-4">Tasks App</h1>
       </Link>
